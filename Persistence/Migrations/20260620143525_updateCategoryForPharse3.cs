eb737d042aae050b308b0069f6651929f54c255f
Balerion934 <tuan.nhokprooo@gmail.com>
2026-07-03 01:02:45 +0700

    Update Migration Data

---
+++ Persistence/Migrations/20260620143525_updateCategoryForPharse3.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateCategoryForPharse3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_Slug",
+                table: "Categories");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_Slug",
+                table: "Categories",
+                column: "Slug",
+                unique: true,
+                filter: "[Slug] IS NOT NULL AND [IsDeleted] = 0");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_Slug",
+                table: "Categories");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_Slug",
+                table: "Categories",
+                column: "Slug",
+                unique: true);
+        }
+    }
+}
